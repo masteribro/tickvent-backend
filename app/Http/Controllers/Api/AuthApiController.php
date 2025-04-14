06be24764d64ed3cefb6fe1850d276f0c8d58480
Blessing Sanusi <holuwoleh@gmail.com>
2025-04-14 22:32:14 +0100

    return user data

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -51,7 +51,7 @@ class AuthApiController extends Controller
         $resp = UserService::registerUser(request('email'));
 
         if($resp["status"]) {
-            return ResponseHelper::successResponse($resp["message"]);
+            return ResponseHelper::successResponse($resp["message"], $resp['data']);
         }
 
         return ResponseHelper::errorResponse($resp["message"]);
