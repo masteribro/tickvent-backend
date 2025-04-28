1c921ee761189a450e6ba078b2a38932c928c21e
Blessing Sanusi <holuwoleh@gmail.com>
2025-04-28 22:24:28 +0100

    added password verification endpoint

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -617,7 +617,7 @@ class AuthApiController extends Controller
                 } else {
                     return response()->json([
                         'status' => false
-                    ], 200);
+                    ], 422);
                 }
 
         } catch (\Exception $e) {
